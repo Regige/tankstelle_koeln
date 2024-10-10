2f908ed65d256f7805b8e0e4a8c0166b5104dc20
Regige <144334703+Regige@users.noreply.github.com>
2024-10-10 20:57:23 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,66 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'tankstelle_koeln';
+
+  //  $(document).ready(function () {
+  //       var map = document.getElementById("map"),
+  //         list = document.getElementById("list"),
+  //         listUl = list.querySelector("ul"),
+  //         mapBtn = document.getElementById("mapBtn"),
+  //         listBtn = document.getElementById("listBtn");
+  //       var mapObject;
+
+  //       function toggleHandler(toggle) {
+  //         if (toggle) {
+  //           styleHandler(map, mapBtn, list, listBtn);
+  //         } else {
+  //           styleHandler(list, listBtn, map, mapBtn);
+  //         }
+  //       }
+
+  //       function styleHandler(block, blockBtn, none, noneBtn) {
+  //         none.style.display = "none";
+  //         noneBtn.style.backgroundColor = "#ccc";
+  //         block.style.display = "block";
+  //         blockBtn.style.backgroundColor = "#666";
+  //       }
+
+  //       function init() {
+  //         toggleHandler(true);
+  //         mapBtn.addEventListener("click", toggleHandler.bind(null, true));
+  //         listBtn.addEventListener("click", toggleHandler.bind(null, false));
+
+  //         mapObject = L.map("map").setView([50.937231, 6.960279], 16);
+
+  //         L.tileLayer("http://{s}.tiles.koeln.de/tiles/kde/{z}/{x}/{y}.png", {
+  //           attribution:
+  //             '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+  //         }).addTo(mapObject);
+
+  //         var url =
+  //           "https://geoportal.stadt-koeln.de/arcgis/rest/services/Gefahrgutstrecken/MapServer/0/query?where=objectid%20is%20not%20null&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=%2A&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson";
+
+  //         $.ajax({
+  //           url: url,
+  //           dataType: "jsonp",
+  //         }).done(function (data) {
+  //           $.each(data.features, addItem);
+  //         });
+  //       }
+
+  //       function addItem(index, value) {
+  //         var name = value.attributes.ADRESSE;
+  //         var longitude = value.geometry.y;
+  //         var latitude = value.geometry.x;
+
+  //         L.marker([longitude, latitude]).addTo(mapObject).bindPopup(name);
+
+  //         listEl = document.createElement("li");
+  //         text = document.createTextNode("Name: " + name);
+  //         listEl.appendChild(text);
+  //         listUl.appendChild(listEl);
+  //       }
+
+  //       init();
+  //     });
 }
